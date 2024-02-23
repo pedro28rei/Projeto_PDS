faea8b9be3aa5116d8f906f05be315efe33c7c68
Pedro Rei <120483256+pedro28rei@users.noreply.github.com>
2024-02-23 01:24:03 +0000

    Criação do projeto no Visual Studios

---
+++ Projeto/TP_PDS/Main/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+* -Descrição: Cração do Projeto
+* -Autor: Pedro Rei
+* -Contacto: pedro28rei@gmail.com
+* -Data: 23/02/2024
+* LESI - PDS
+* 
+*/
+
+using Objetos;
+
+namespace Main
+{
+    /// <summary>
+    /// Classe onde se encontra o programa
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Projeto/TP_PDS/Objetos/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿/*
+* -Descrição: Cração da classe Pessoa
+* -Autor: Pedro Rei
+* -Contacto: pedro28rei@gmail.com
+* -Data: 23/02/2024
+* LESI - PDS
+* 
+*/
+
+namespace Objetos
+{
+    public class Pessoa
+    {
+    }
+}
